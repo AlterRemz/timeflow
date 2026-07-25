734fb992cdee80355672b73c6f6ea23443e1a9ee
AlterRemz <remkanaeru@gmail.com>
2026-07-25 15:55:37 +0700

    Feat(Finance): added a save function to the income and expense buttons

--- JS/finance.js
+++ JS/finance.js
@@ -1,8 +1,20 @@
 let balance = Number(localStorage.getItem("balance")) || 1000;
 
+let mode = "";
+
 const balanceText = document.getElementById("balance");
 
 const incomeBtn = document.getElementById("incomeBtn");
+
+const expenseBtn = document.getElementById("expenseBtn");
+
+const modal = document.getElementById("modal");
+
+const cancelBtn = document.getElementById("cancelBtn");
+
+const saveBtn = document.getElementById("saveBtn");
+
+const amountInput = document.getElementById("amountInput");
 
 function tampilkanSaldo() {
   balanceText.innerHTML = `Rp ${balance.toLocaleString("id-ID")}<span class="cents">.00</span>`;
@@ -11,14 +23,43 @@ function tampilkanSaldo() {
 tampilkanSaldo();
 
 incomeBtn.addEventListener("click", () => {
-  const nominal = Number(prompt("Masukkan nominal income"));
 
-  if (!nominal || nominal <= 0) return;
+  mode = "income";
 
-  balance += nominal;
-
-localStorage.setItem("balance", balance);
-
-  tampilkanSaldo();
+  modal.style.display = "flex"
 
 })
+
+cancelBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+saveBtn.addEventListener("click", () => {
+  const nominal = Number(amountInput.value);
+
+  if (!nominal || nominal <= 0) return;
+  
+  if (mode === "income") {
+    balance += nominal;
+  } 
+  else if (mode === "expense") {
+    balance -= nominal;
+  }
+
+localStorage.setItem("balance", balance);
+  
+  tampilkanSaldo();
+  
+  amountInput.value = "";
+
+  modal.style.display = "none";
+});
+
+expenseBtn.addEventListener("click", () => {
+
+  mode = "expense";
+
+  modal.style.display = "flex";
+  
+});
+
